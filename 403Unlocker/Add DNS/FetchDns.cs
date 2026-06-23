6c45abc797beca226b04a4341fedf3e01ddbb240
ALi Moradzade <alimooradzade@gmail.com>
2026-06-24 01:16:41 +0330

    optimized

--- 403Unlocker/Add DNS/FetchDns.cs
+++ 403Unlocker/Add DNS/FetchDns.cs
@@ -1,113 +1,69 @@
 using _403Unlocker.Data_Models;
 using HtmlAgilityPack;
+using Network_Utilities.Http_Service;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using Network_Utilities.Http_Service;
+using System.Xml;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace _403Unlocker.Add_DNS
 {
     internal static class FetchDns
     {
+        //https://www.getflix.com.au/setup/dns-servers/
+        //https://www.publicdns.xyz
+
         private static readonly Uri url = new Uri("https://publicdns.xyz");
 
         public async static Task<DnsConfig> ScrapDnsServersAsync()
         {
-            //https://www.getflix.com.au/setup/dns-servers/
-            //https://www.publicdns.xyz
+            List<DnsInfo> dnsInfos = new List<DnsInfo>();
 
             HtmlDocument htmlDocument = new HtmlDocument();
             HttpResult response = await HttpService.SendRequestAsync(url);
-
             htmlDocument.LoadHtml(response.HttpResponseContent);
 
-            HtmlNode table = ParseToTable(htmlDocument);
+            HtmlNode table = htmlDocument.DocumentNode.SelectSingleNode("//table");
 
-            var rows = ExtractRows(table);
+            HtmlNode tbody = table.SelectSingleNode("./tbody");
 
-            rows = rows.Skip(1).ToArray(); // removes table title
+            HtmlNodeCollection rows = tbody.SelectNodes("./tr");
 
-            rows = RemoveIPv6(rows);
-
-            string[][] ipv4 = TrimIPv4(rows);
-
-            DnsInfo[][] dnsInfos = ConvertToDnsInfo(ipv4);
-            DnsConfig dnsConfig = new DnsConfig(dnsInfos.SelectMany(dns => dns).ToList());
-            return dnsConfig;
-        }
-
-        private static HtmlNode ParseToTable(HtmlDocument htmlDocument)
-        {
-            var table = htmlDocument.DocumentNode.SelectSingleNode("//table");
-            return table;
-        }
-
-        private static HtmlNode[][] ExtractRows(HtmlNode htmlNode)
-        {
-            HtmlNodeCollection rows = htmlNode.SelectNodes(".//tr");
-
-            HtmlNode[][] customizedRows = rows.Select
-            (
-                row => row.ChildNodes.Where
-                    (
-                        cell => cell.Name != "#text"
-                    ).ToArray()
-            ).ToArray();
-
-            return customizedRows;
-        }
-
-        private static HtmlNode[][] RemoveIPv6(HtmlNode[][] rows)
-        {
-            HtmlNode[][] removedIPv6 =  rows.Where(x => x.Count() == 3).ToArray();
-            return removedIPv6;
-        }
-
-        private static string[][] TrimIPv4(HtmlNode[][] rows)
-        {
-            var minedDns = rows.Select
-            (
-                row => row.Select
-                (
-                    cell => string.Concat
-                    (
-                        cell.InnerText.Where
-                        (
-                            c => !char.IsControl(c)
-                        )
-                    )
-                ).ToArray()
-            ).ToArray();
-
-            return minedDns;
-        }
-
-        private static DnsInfo[][] ConvertToDnsInfo(string[][] minedDns)
-        {
-            DnsInfo[][] dnsInfosList = minedDns.Select(dnsInfo =>
+            foreach (HtmlNode row in rows)
             {
-                string provider = dnsInfo[0];
-                string primary = dnsInfo[1];
-                string secondary = dnsInfo[2];
-
-                List<DnsInfo> dnsInfos = new List<DnsInfo>();
-                if (!string.IsNullOrEmpty(primary))
+                if (row.SelectSingleNode("./th") != null)
                 {
-                    dnsInfos.Add(new DnsInfo(IPAddress.Parse(primary), provider));
+                    string provider = row.SelectSingleNode("./th").SelectSingleNode("./a").InnerText;
+                    HtmlNodeCollection nodes = row.SelectNodes("./td");
+                    if (nodes != null)
+                    {
+                        foreach (HtmlNode dns in nodes)
+                        {
+                            HtmlNode node = dns.SelectSingleNode("./span");
+                            if (node != null)
+                            {
+                                string text = node.InnerText;
+                                if (IPAddress.TryParse(text, out IPAddress ip) && ip.AddressFamily == AddressFamily.InterNetwork)
+                                {
+                                    dnsInfos.Add(new DnsInfo(ip, provider));
+                                }
+                            }
+                        }
+                    }
                 }
-                if (!string.IsNullOrEmpty(secondary))
-                {
-                    dnsInfos.Add(new DnsInfo(IPAddress.Parse(secondary), provider));
-                }
-                return dnsInfos.ToArray();
-            }).ToArray();
+            }
 
-            return dnsInfosList;
+            DnsConfig dnsConfig = new DnsConfig(dnsInfos);
+            return dnsConfig;
         }
     }
 }
